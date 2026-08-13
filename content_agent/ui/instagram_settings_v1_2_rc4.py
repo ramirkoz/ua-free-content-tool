5a240f0daca7f223737f39ad106b4b03939b27ea
Tony <ramirua.fm@gmail.com>
2026-08-13 18:40:30 +0300

    Prepare RC4 Instagram settings mixin

--- content_agent/ui/instagram_settings_v1_2_rc4.py
+++ content_agent/ui/instagram_settings_v1_2_rc4.py
@@ -1,4 +1,13 @@
 from __future__ import annotations
 
+import threading
+import tkinter as tk
+from tkinter import ttk
+
+from ..instagram_api_v1_2_rc4 import inspect_instagram_profile
+
+
 class InstagramSettingsMixin:
-    pass
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        super().__init__(*args, **kwargs)  # type: ignore[misc]
+        self._upgrade_instagram_settings()
