52dcff4c81973b82ae7ac8d6141705b25e7c395c
Tony <ramirua.fm@gmail.com>
2026-08-13 18:42:53 +0300

    Add Instagram settings mixin to RC4 UI

--- content_agent/ui/v1_2_rc4_window.py
+++ content_agent/ui/v1_2_rc4_window.py
@@ -7,10 +7,11 @@ from tkinter import ttk
 from ..multi_image_store_v1_2_rc4 import MultiImageStore
 from ..publisher_factory_v1_2_rc3_compat import Rc3CompatiblePublisherFactory
 from ..worker_v1_2_rc4 import Rc4PublicationWorker
+from .instagram_settings_v1_2_rc4 import InstagramSettingsMixin
 from .v1_2_rc3_final_window import MainWindow as RC3FinalWindow
 
 
-class MainWindow(RC3FinalWindow):
+class MainWindow(InstagramSettingsMixin, RC3FinalWindow):
     def __init__(self, *args: object, **kwargs: object) -> None:
         self.multi_image_store = MultiImageStore()
         super().__init__(*args, **kwargs)
