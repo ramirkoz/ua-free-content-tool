d40835b1d6542cb72469ce816e8590c078b035fe
Tony <ramirua.fm@gmail.com>
2026-08-13 18:52:22 +0300

    Wire RC4 secondary image registration action

--- content_agent/ui/multi_image_actions_v1_2_rc4.py
+++ content_agent/ui/multi_image_actions_v1_2_rc4.py
@@ -1,4 +1,15 @@
 from __future__ import annotations
 
+from ..image_registration_v1_2_rc4 import register_secondary_image
+
+
 class MultiImageActionsMixin:
-    pass
+    def register_extra_image(self, upload, group_id: int) -> None:
+        register_secondary_image(
+            self.db,
+            self.managed_media_registry,
+            self.multi_image_store,
+            upload,
+            group_id,
+        )
+        self._refresh_attached_images()
